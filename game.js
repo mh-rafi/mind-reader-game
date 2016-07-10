03746436c1b5066cad6ee67f0f8d241c79dc5b33
MH Rafi <rafi.bogra@gmail.com>
2016-07-10 19:30:45 +0600

    problem fixed

--- game.js
+++ game.js
@@ -1,25 +1,30 @@
 (function($) {
+	'use strict';
+	$(window).load(function() {
+		$('.preloader').delay(500).fadeOut();
+	});
+
 	var icons = [
-		'fa-asterisk', 
-		'fa-at', 
-		'fa-balance-scale', 
-		'fa-bar-chart', 
-		'fa-barcode', 
-		'fa-bars', 
-		'fa-beer', 
-		'fa-bed', 
-		'fa-behance', 
-		'fa-bell', 
-		'fa-binoculars', 
-		'fa-birthday-cake', 
-		'fa-bitbucket', 
-		'fa-bluetooth', 
-		'fa-bomb', 
-		'fa-book', 
-		'fa-bolt', 
-		'fa-bookmark', 
-		'fa-bug', 
-		'fa-camera', 
+		'fa-asterisk',
+		'fa-at',
+		'fa-balance-scale',
+		'fa-bar-chart',
+		'fa-barcode',
+		'fa-bars',
+		'fa-beer',
+		'fa-bed',
+		'fa-behance',
+		'fa-bell',
+		'fa-binoculars',
+		'fa-birthday-cake',
+		'fa-bitbucket',
+		'fa-bluetooth',
+		'fa-bomb',
+		'fa-book',
+		'fa-bolt',
+		'fa-bookmark',
+		'fa-bug',
+		'fa-camera',
 		'fa-briefcase',
 		'fa-chrome',
 		'fa-codepen',
@@ -31,12 +36,14 @@
 		'fa-dropbox',
 		'fa-dribbble',
 		'fa-desktop'
-		];
+	];
 	// console.log(icons.length);
 	var $all_icons = $('.all_icons');
 	var $result_icon = $('.result_icon');
 	var $btnTry = $('.try_again');
 	var $resultText = $('.result_text');
+	var $resultLoader = $('.result_loader');
+	var isResult = false;
 	var prev_rand;
 	var resultIconIndex;
 
@@ -65,30 +72,36 @@
 		$result_icon.find('span').removeAttr('class');
 		$btnTry.hide();
 		$resultText.hide();
+		$resultLoader.hide();
 	}
 
 	var showResult = function(e) {
 		$result_icon.find('span').removeAttr('class');
-		$result_icon.find('span').addClass('fa ' + icons[resultIconIndex]);
+		$resultLoader.show();
 		$all_icons.html('');
-		$btnTry.show();
-		$resultText.show();
+
+		setTimeout(function() {
+			$btnTry.show();
+			$resultText.show();
+			$resultLoader.hide();
+			$result_icon.find('span').addClass('fa ' + icons[resultIconIndex]);
+		}, 1500);
 	}
 
 	$result_icon.on('click', showResult);
 	$btnTry.on('click', renderIcons);
 	renderIcons();
 
-	/*------------ Pop up--------------*/
+	/*------------ Pop up --------------*/
 	$('.header .btn').on('click', function(e) {
 		var dataDiv = $(this).data('div');
 		$('.popup').hide();
-		$('#'+ dataDiv).fadeIn();
-		console.log(dataDiv);
+		$('#' + dataDiv).fadeIn();
+		// console.log(dataDiv);
 	});
 	$('.popup_close').on('click', function(e) {
 		e.preventDefault();
 		$(this).parent('.popup').fadeOut();
 	});
-	
+
 })(jQuery)
